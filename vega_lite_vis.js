a8c2ff285b8184d7a56ace8a47774cfe98f4e8c0
Patty Hung <67167072+Patty-tw@users.noreply.github.com>
2022-10-09 21:34:18 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,10 @@
 var vg_1 = "js/map.vg.json";
+var vg_1 = "js/bubble_chart.vg.json"
 
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#bubble_chart", vg_2).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
