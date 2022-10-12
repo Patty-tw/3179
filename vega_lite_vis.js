d346a8ba9f04db75f08093f0354c451edf8f1f84
Patty Hung <67167072+Patty-tw@users.noreply.github.com>
2022-10-12 12:41:27 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,10 +1,21 @@
-var vg_1 = "js/map.vg.json";
+var vg_1 = "js/interactive_line_chart_V2.vg.json"
 var vg_2 = "js/bubble_chart.vg.json"
+var vg_3 = "js/macroplastic.vg.json"
+var vg_4 = "js/microplastic.vg.json"
 
-vegaEmbed("#choropleth_map", vg_1).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+vegaEmbed("#line_chart", vg_1).then(function(result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
 
 vegaEmbed("#bubble_chart", vg_2).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+vegaEmbed("#macroplastic", vg_3).then(function(result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+vegaEmbed("#microplastic", vg_4).then(function(result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
