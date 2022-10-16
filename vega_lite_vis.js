df667d72591ab94d51148eedd3aa466c0da293af
Patty Hung <67167072+Patty-tw@users.noreply.github.com>
2022-10-17 02:00:09 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,7 @@
 var vg_1 = "js/improve_map.vg.json"
 var vg_2 = "js/bar_chart.vg.json"
-var vg_3 = "js/line_chart.vg.json"
+var vg_3 = "js/macroplastic.vg.json"
+var vg_4 = "js/microplastic.vg.json"
 
 vegaEmbed("#map", vg_1, { "actions": false }).then(function(result) {
     // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
@@ -10,6 +11,10 @@ vegaEmbed("#bar_chart", vg_2, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
 
-vegaEmbed("#line_chart", vg_3, { "actions": false }).then(function(result) {
-    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-    }).catch(console.error);
+
+vegaEmbed("#macroplastic", vg_3, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);    
+
+vegaEmbed("#microplastic", vg_4, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
