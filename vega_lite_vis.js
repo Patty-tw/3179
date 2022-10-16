2b29770934a65a15c6ee4867ec6d640fecc82970
Patty Hung <67167072+Patty-tw@users.noreply.github.com>
2022-10-17 03:15:13 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -18,3 +18,4 @@ vegaEmbed("#macroplastic", vg_3, { "actions": false }).then(function(result) {
 
 vegaEmbed("#microplastic", vg_4, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
